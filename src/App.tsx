2f5706d9bf9dc609d8db3d82b23d5d681a7ae2d8
Dorin zrihen <zrihend@gmail.com>
2024-12-09 16:43:40 +0200

    netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { getNextThreeEvents } from "./utils";
 import { useTranslation } from "react-i18next";
 import TimezoneSelector from "./TimezoneSelector";
